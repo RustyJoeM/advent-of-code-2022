f0cf721624589c2390871839a89881b27daba1e2
Jozef Miklos <rusty.joe.mik@gmail.com>
2022-12-01 19:55:59 +0100

    fix separation of parse + computing for d01

--- src/d01.rs
+++ src/d01.rs
@@ -3,23 +3,23 @@ const DAY_ID: utils::DayIdType = 1;
 
 type Res = u32;
 
-fn parse_input(data: &str) -> Vec<u32> {
+fn parse_input(data: &str) -> Vec<Vec<u32>> {
     let mut res = vec![];
 
     for group in data.split("\n\n") {
-        let s: u32 = group.lines().map(|x| x.parse::<u32>().unwrap()).sum();
+        let s = group.lines().map(|x| x.parse::<u32>().unwrap()).collect();
         res.push(s);
     }
 
     res
 }
 
-fn solve_part1(data: &[u32]) -> Res {
-    *data.iter().max().unwrap()
+fn solve_part1(data: &[Vec<u32>]) -> Res {
+    data.iter().map(|v| v.iter().sum()).max().unwrap()
 }
 
-fn solve_part2(data: &[u32]) -> Res {
-    let mut v = data.to_vec();
+fn solve_part2(data: &[Vec<u32>]) -> Res {
+    let mut v: Vec<u32> = data.iter().map(|v| v.iter().sum()).collect();
     v.sort();
     v.iter().rev().take(3).sum()
 }
